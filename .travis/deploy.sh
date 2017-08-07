f0ac6a311c7430c32c8a4da11a7b6466547e8ea1
Anton Parkhomenko <mailbox@chuwy.me>
2017-08-07 21:51:31 +0700

    Fix deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
 tag=$1
-project=snowplow_snowflake_transformer
-
-slashed="$project/"
-slashed_len=${#slashed}
-
-cicd=${tag:0:${slashed_len}}
-release=${tag:${slashed_len}}
-
-if [ "${cicd}" == "${slashed}" ]; then
-    if [ "${release}" == "" ]; then
-        echo "Warning! No release specified! Ignoring."
-        exit 2
-    fi
-else
-    echo "This can't be deployed - there's no $slashed tag! (is the travis condition set?)"
-    exit 1
-fi
 
 cd $TRAVIS_BUILD_DIR
 
-export TRAVIS_BUILD_RELEASE_TAG=${release}
+export TRAVIS_BUILD_RELEASE_TAG=${tag}
 release-manager --config ./.travis/release.yml --check-version --make-artifact --upload-artifact
